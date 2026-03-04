adc738e699b2d0a2f188cabebf083ad85f35bc29
haogu <haofgu@gmail.com>
2026-03-04 00:08:23 -0800

    removed vcpkg for real

--- Project3/Lemmings/vcpkg_installed/x64-windows/vcpkg/blds/libflac/x64-windows-dbg/src/share/getopt/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/CS32/Project3/Lemmings/vcpkg_installed/x64-windows/vcpkg/blds/libflac/src/1.5.0-5ec0185c23.clean/src/share/getopt
-# Build directory: C:/CS32/Project3/Lemmings/vcpkg_installed/x64-windows/vcpkg/blds/libflac/x64-windows-dbg/src/share/getopt
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- Project3/Lemmings/vcpkg_installed/x64-windows/vcpkg/blds/libflac/x64-windows-dbg/src/share/replaygain_synthesis/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/CS32/Project3/Lemmings/vcpkg_installed/x64-windows/vcpkg/blds/libflac/src/1.5.0-5ec0185c23.clean/src/share/replaygain_synthesis
-# Build directory: C:/CS32/Project3/Lemmings/vcpkg_installed/x64-windows/vcpkg/blds/libflac/x64-windows-dbg/src/share/replaygain_synthesis
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
